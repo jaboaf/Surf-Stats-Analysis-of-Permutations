d7f523f513aa5081986eb44056667a4b47b99ab9
jaboaf <50273977+jaboaf@users.noreply.github.com>
2021-04-22 09:33:33 -0400

    updated contingency table to include class

--- src/ContingencyTable.jl
+++ src/ContingencyTable.jl
@@ -1,5 +1,5 @@
 using JSON
-doc = JSON.parse( open("Data/CleanAllDataCC.txt", "r"))
+doc = JSON.parse( open("data/CleanAllDataCC.txt", "r"))
 
 filter!(doc) do wave
 	# Simplify data to most "complete years"
@@ -16,7 +16,7 @@ for id in EvtIds
     maxRnd[id] = maximum(map(wid->Base.parse(Int,doc[wid]["rnd"]),thisEvt))
 end
 
-@enum Orig AUS=1 BRA ESP FRA PRT USA ZAF IDN ITA JPN NZL
+@enum Orig AUS=1 BRA=2 ESP=3 FRA=4 PRT=5 USA=6 ZAF=7 IDN=8 ITA=9 JPN=10 NZL=11
 isoDict = Dict([
     "Australia" => AUS,
     "Brazil" => BRA,
@@ -43,18 +43,34 @@ OrigInd(S::Symbol) = findfirst(x->Symbol(x)==S, instances(Orig))
 # ATH_orig ∈ {:AUS,:BRA,:FRA,:IDN,:ITA,:JPN,:NZL,:PRT,:USA,:ZAF}
 # JUD_orig ∈ {:AUS,:BRA,:ESP,:FRA,:PRT,:USA,:ZAF}
 # JUD_score ∈ {0.1, 0.2, ..., 9.8, 9.9, 10.0}
+# JUD_cls ∈ {1,2,3,4,5}
 
-vars = [:YR,:EVT,:RND,:HEAT,:ATH_orig,:JUD_orig,:JUD_score]
-data = zeros(Int8,(2,10,7,16,11,7,100))
+vars = [:YR,:EVT,:RND,:HEAT,:ATH_orig,:JUD_orig,:JUD_score,:JUD_cls]
+data = zeros(Rational,(2,10,7,16,11,7,100,5))
 for wave in doc
 	yr = (wave[2]["evtYear"]=="2018" ? 1 : 2)
 	evt = EvtInd(wave[2]["evtName"])
-	rnd = maxRnd[wave[2]["evtId"]] - Base.parse(Int,wave[2]["rnd"]) + 1
+	rnd = 7-abs(Base.parse(Int,wave[2]["rnd"])-maxRnd[wave[2]["evtId"]])
 	ht = Base.parse(Int,wave[2]["heat"])
 	ath_orig = Int(isoDict[wave[2]["athOrig"]])
-	for i in 1:5
+	#=
+    for i in 1:5
 		jud_orig = Int(isoDict[ wave[2]["subScoOrig"][i] ])
 		jud_score = Int(round(wave[2]["subSco"][i], digits=1)*10)
 		data[yr,evt,rnd,ht,ath_orig,jud_orig,jud_score] += 1
 	end
+    =#
+    jud_scores = Float16.(wave[2]["subSco"])
+    jud_origs = map(c->isoDict[c], wave[2]["subScoOrig"])
+    
+    for (cls,score) in enumerate(sort(unique(jud_scores)))
+        I = findall(x->x==score, jud_scores)
+        jud_cls = Int(cls) # to reassure julia's type inference
+        Sym(jud_origs[I])
+        for i in I
+            jud_orig = Int(jud_origs[i])
+            jud_score = Int(round(jud_scores[i], digits=1)*10)
+            data[yr,evt,rnd,ht,ath_orig,jud_orig,jud_score,jud_cls] += 1
+        end
+    end
 end
